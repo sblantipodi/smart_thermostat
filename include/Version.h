f62543fa20abf92f07d7415e1b583775231b33a1
Davide Perini <perini.davide@dpsoftware.org>
2020-06-07 16:58:54 +0200

    PlatformIO Version Increment update (v0.1.4)

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "6.6.20"
+        #define VERSION "6.6.22"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-06-06 18:44:47.265427"
+        #define BUILD_TIMESTAMP "2020-06-07 16:57:51.358141"
     #endif
     
