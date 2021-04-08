e2aeb5bbf15bd67b0256cb513fc0c81bda4cab4c
Davide Perini <perini.davide@dpsoftware.org>
2021-04-08 18:36:41 +0200

    Reduced memory footprint

--- include/Version.h
+++ include/Version.h
@@ -4,6 +4,6 @@
         #define VERSION "6.11.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-04-08 18:27:25.257174"
+        #define BUILD_TIMESTAMP "2021-04-08 18:36:05.447695"
     #endif
     
