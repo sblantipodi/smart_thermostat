aec2fc47e1091c2092ea91e214577f4a0f1683f5
Davide Perini <perini.davide@dpsoftware.org>
2023-07-31 14:32:57 +0200

    CLion does not need CMakeListsUser.txt anymore

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "6.14.33"
+        #define VERSION "6.14.38"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-07-16 19:54:31.533304"
+        #define BUILD_TIMESTAMP "2023-07-31 14:32:07.579435"
     #endif
     
