c2ce4c9c61f4bb0e3d233580b9782d8be6ec54cf
Davide Perini <perini.davide@dpsoftware.org>
2024-07-05 11:19:38 +0200

    beep toggle

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "6.15.71"
+        #define VERSION "6.15.73"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-07-05 11:14:15.512648"
+        #define BUILD_TIMESTAMP "2024-07-05 11:19:17.687099"
     #endif
     
