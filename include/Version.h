dffc427108a02ca762f69d7cff37e90e5ea6f621
Davide Perini <perini.davide@dpsoftware.org>
2026-05-02 22:34:39 +0200

    arduino_bootstrapper platformio_version_increment update

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "7.0.132"
+        #define VERSION "7.1.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-05-02 22:18:51.948801"
+        #define BUILD_TIMESTAMP "2026-05-02 22:34:25.466598"
     #endif
     
