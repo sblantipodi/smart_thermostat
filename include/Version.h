b6d60b1e95d9dcc77c4443f77a4b5537653e882b
Davide Perini <perini.davide@dpsoftware.org>
2026-07-27 11:01:37 +0200

    ; Espressif32 -> 55.03.311, Arduino Core ->	3.3.11, based on ESP-IDF -> 5.5.5

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "7.1.2"
+        #define VERSION "7.1.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-05-02 22:34:25.466598"
+        #define BUILD_TIMESTAMP "2026-07-27 11:00:27.513526"
     #endif
     
